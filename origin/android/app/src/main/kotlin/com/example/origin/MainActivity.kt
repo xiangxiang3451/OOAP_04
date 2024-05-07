3520c7f5463dc4f411495c8931a59f54cabddfd8
Chang <xiangxiang3451@gmail.com>
2024-05-08 00:33:51 +0700

    change

--- origin/android/app/src/main/kotlin/com/example/origin/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.origin
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- service_stub/android/app/src/main/kotlin/com/example/service_stub/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.service_stub
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
